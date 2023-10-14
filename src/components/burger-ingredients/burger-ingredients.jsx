5938294a491bca92a15c6ae03cd6dbd1364dd32a
hlopoq <grenlandialol@gmail.com>
2023-10-14 13:23:15 +0300

    добавлена прокрутка

--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import PropTypes from "prop-types";
 import { Tab } from "@ya.praktikum/react-developer-burger-ui-components";
 import style from "./burger-ingredients.module.css";
@@ -9,8 +9,14 @@ import { ingredientPropType } from "../../utils/prop-types";
 
 export default function BurgerIngredients({ data }) {
   const [modalActive, setModalActive] = useState(false);
-  const [currentCategory, setCurrentCategory] = useState("bun");
   const [selectedIngredient, setSelectedIngredient] = useState(null);
+  const [activeCategory, setActiveCategory] = useState("bun");
+  const scrollContainerRef = useRef(null);
+  const tabsRef = {
+    bun: useRef(null),
+    sauce: useRef(null),
+    main: useRef(null),
+  };
 
   const handleOpen = (ingredient) => {
     setSelectedIngredient(ingredient);
@@ -32,12 +38,44 @@ export default function BurgerIngredients({ data }) {
     return data.filter((ingredient) => ingredient.type === category);
   };
 
-  const scrollToCategory = (category) => {
-    const element = document.querySelector(`#${category}`);
-    if (element) {
-      element.scrollIntoView({ behavior: "smooth" });
+  const handleTabClick = (category) => {
+    setActiveCategory(category);
+    tabsRef[category].current.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  };
+
+  const handleScroll = () => {
+    if (scrollContainerRef.current) {
+      const scrollY = scrollContainerRef.current.scrollTop;
+      const containerTop =
+        scrollContainerRef.current.getBoundingClientRect().top;
+
+      for (const category of categories) {
+        if (tabsRef[category.key].current) {
+          const tabTop =
+            tabsRef[category.key].current.getBoundingClientRect().top -
+            containerTop;
+
+          if (scrollY >= tabTop) {
+            setActiveCategory(category.key);
+          }
+        }
+      }
     }
   };
+
+  useEffect(() => {
+    if (scrollContainerRef.current) {
+      scrollContainerRef.current.addEventListener("scroll", handleScroll);
+    }
+    return () => {
+      if (scrollContainerRef.current) {
+        scrollContainerRef.current.removeEventListener("scroll", handleScroll);
+      }
+    };
+  }, []);
 
   return (
     <section className={style.burgerIngredients}>
@@ -47,23 +85,20 @@ export default function BurgerIngredients({ data }) {
           <Tab
             key={category.key}
             value={category.key}
-            active={currentCategory === category.key}
-            onClick={() => {
-              setCurrentCategory(category.key);
-              scrollToCategory(category.key);
-            }}
+            active={activeCategory === category.key}
+            onClick={() => handleTabClick(category.key)}
           >
             {category.label}
           </Tab>
         ))}
       </div>
-      <div className={`custom-scroll ${style.scrollIngredients}`}>
+      <div
+        ref={scrollContainerRef}
+        className={`custom-scroll ${style.scrollIngredients}`}
+      >
         {categories.map((category) => (
-          <div key={category.key}>
-            <h2
-              id={category.key}
-              className="text text_type_main-medium pt-5 pb-5"
-            >
+          <div key={category.key} ref={tabsRef[category.key]}>
+            <h2 className="text text_type_main-medium pt-5 pb-5">
               {category.label}
             </h2>
             <ul className={`${style.list} pt-5 pb-5`}>
